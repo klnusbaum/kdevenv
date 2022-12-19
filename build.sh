b4cf93a406f707ce1a4f1c2ee00c3e71122b8422
Kurtis Nusbaum <kcommiter@gmail.com>
2022-12-19 08:51:35 -0800

    add a "version" to kdevenv. we'll see if I actually use it

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 set euo -pipefail
 
 source "$(dirname ${BASH_SOURCE})/lib.sh"
+source "$(dirname ${BASH_SOURCE})/version.sh"
 
 if [ -z "$(docker volume ls --filter name=$HOME_VOLUME --format {{.Name}})" ];
 then
@@ -12,4 +13,4 @@ echo "BUILDING WITH USER: $CONTAINER_USER"
 docker build \
     --build-arg CONTAINER_USER="$CONTAINER_USER" \
     --build-arg CONTAINER_SHELL="$CONTAINER_SHELL" \
-    -t kdevenv:1.0 .
+    -t "kdevenv:${KDEVENV_VERSION}" .
