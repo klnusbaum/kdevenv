510929fd178fd3caf5d9e374f6d64432700dcb8a
Kurtis Nusbaum <kcommiter@gmail.com>
2023-01-09 23:46:52 -0500

    do a docker pull if arch is out of date

--- build.sh
+++ build.sh
@@ -9,6 +9,16 @@ then
     docker volume create $HOME_VOLUME &> /dev/null
 fi
 
+FIVE_DAYS_AGO=$(date --date="5 days ago" "+%Y-%m-%d")
+ARCH_CREATED_AT=$(docker images  --format "{{ .CreatedAt }}" archlinux:base | awk '{print $1}')
+
+FIVE_DAYS_AGO_UNIX=$(date -d $FIVE_DAYS_AGO +%s)
+ARCH_CREATED_AT_UNIX=$(date -d $ARCH_CREATED_AT +%s)
+
+if [ $FIVE_DAYS_AGO_UNIX -ge $ARCH_CREATED_AT_UNIX ]; then
+    docker pull archlinux:base
+fi
+
 docker build \
     --build-arg CONTAINER_USER="$CONTAINER_USER" \
     --build-arg CONTAINER_SHELL="$CONTAINER_SHELL" \
