49db54d068de99d1164b7920e487be92da8a7d85
Kurtis Nusbaum <kcommiter@gmail.com>
2024-02-23 10:40:05 -0800

    Improve restart script

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-SCRIPT_DIR="$(dirname ${BASH_SOURCE})"
+SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
+source "$SCRIPT_DIR/lib.sh"
 
-$SCRIPT_DIR/stop.sh
-$SCRIPT_DIR/start.sh
+"$SCRIPT_DIR/stop.sh"
+docker wait "$CONTAINER_NAME" 2>/dev/null || true
+"$SCRIPT_DIR/start.sh"
